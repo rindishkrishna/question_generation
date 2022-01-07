7df0cd70e3cf983c320adf526f8fb6627f30baa9
rindish krishna <rindishkrishna@gmail.com>
2022-01-07 20:39:03 +0530

    added description

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9aaf1bfb8c229a8c197aee6cba807bf6ec614d78a5e04afb3985eb9c744da983
-size 719
+from flask import Flask,render_template, request
+# import pickle
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  return render_template('index.html')
+
+@app.route('/questions-generated', methods=['POST'])
+def home():
+    if request.method == 'POST':
+      if request.form['generate']:
+        data = request.form['a']
+        # model = pickle.load(open('model.pkl','rb'))
+        # l=model(data)
+        list=[{'answer': 'Python',
+  'question': 'What is an interpreted, high-level, general-purpose programming language?'},
+ {'answer': 'Guido van Rossum', 'question': 'Who created Python?'}]
+        return render_template('answer.html', data=list)
+    else:
+      return    
+
+if __name__ == "__main__":
+  app.run(debug=True) 
