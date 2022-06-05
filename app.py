cd1769bd594f151811fb0f29a0194b5ecc94f6e3
rindish krishna <rindishkrishna@gmail.com>
2022-06-05 20:31:04 +0530

    Qgen endpoint added

--- app.py
+++ app.py
@@ -65,7 +65,65 @@ def main():
 # def hi():
 #   data = request.form['a']
 #   return render_template('error.html')
-
+@app.route('/generate', methods=['POST'])
+def first():
+  json_data = request.get_json()
+  data = json_data["text"]
+  print(data)
+  model = pickle.load(open('model.pkl','rb'))
+  quoted_text = '"""' + data + '"""'
+  Ctext = clean(quoted_text,
+  fix_unicode=True,               # fix various unicode errors
+  to_ascii=True,                  # transliterate to closest ASCII representation
+  lower=False,                     # lowercase text
+  no_line_breaks=False,           # fully strip line breaks as opposed to only normalizing them
+  no_urls=True,                  # replace all URLs with a special token
+  no_emails=False,                # replace all email addresses with a special token
+  no_phone_numbers=False,         # replace all phone numbers with a special token
+  no_numbers=False,               # replace all numbers with a special token
+  no_digits=False,                # replace all digits with a special token
+  no_currency_symbols=False,      # replace all currency symbols with a special token
+  no_punct=False,                 # remove punctuations
+  replace_with_punct="",          # instead of removing punctuations you may replace them
+  replace_with_url="<URL>",
+  replace_with_email="<EMAIL>",
+  replace_with_phone_number="<PHONE>",
+  replace_with_number="<NUMBER>",
+  replace_with_digit="0",
+  replace_with_currency_symbol="<CUR>",
+  lang="en"                       # set to 'de' for German special handling
+  )
+  l=model(Ctext)
+  return {"data":l}
+@app.route('/generate-try-again', methods=['POST'])
+def second():
+  json_data = request.get_json()
+  data = json_data["text"]
+  model = pickle.load(open('model.pkl','rb'))
+  quoted_text = '"""' + data + '"""'
+  Ctext = clean(quoted_text,
+  fix_unicode=True,               # fix various unicode errors
+  to_ascii=True,                  # transliterate to closest ASCII representation
+  lower=False,                     # lowercase text
+  no_line_breaks=False,           # fully strip line breaks as opposed to only normalizing them
+  no_urls=True,                  # replace all URLs with a special token
+  no_emails=False,                # replace all email addresses with a special token
+  no_phone_numbers=False,         # replace all phone numbers with a special token
+  no_numbers=True,               # replace all numbers with a special token
+  no_digits=False,                # replace all digits with a special token
+  no_currency_symbols=False,      # replace all currency symbols with a special token
+  no_punct=False,                 # remove punctuations
+  replace_with_punct="",          # instead of removing punctuations you may replace them
+  replace_with_url="<URL>",
+  replace_with_email="<EMAIL>",
+  replace_with_phone_number="<PHONE>",
+  replace_with_number="<NUMBER>",
+  replace_with_digit="0",
+  replace_with_currency_symbol="<CUR>",
+  lang="en"                       # set to 'de' for German special handling
+  )
+  l=model(Ctext)
+  return {"data":l}
 @app.route('/questions-generated', methods=['POST'])
 def home():
     if request.method == 'POST':
